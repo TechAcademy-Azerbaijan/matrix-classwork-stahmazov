aff7f82f37f47b8fb8ddc6af14d81b768fa12087
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-01 20:02:43 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -2,3 +2,20 @@
 let array = [3, 5, -7, 7, 5, -9, -4]
 
 // Write code here. 
+
+let new_arr = []
+let warning = []
+
+for (let i = 0; i < array.length; i++){
+    start = i + 1
+    last = array.length+1
+    console.log(start, last)
+    if (!array.slice(start,last).includes(array[i]) && !warning.includes(array[i])) {
+        new_arr.push(array[i])
+    }
+    else {
+        warning.push(array[i])
+    }
+}
+
+console.log(...new_arr)
