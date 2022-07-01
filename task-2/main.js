fa5e4c6236d2023766da5219eee4963b9d6e1d6b
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-01 20:08:51 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -2,3 +2,14 @@
 let array = [3, 5, -7, 7, 5, -9, -4]
 
 // Write code here. 
+count = 0
+let new_arr = []
+
+for (let i = 0; i < array.length; i++){
+    if (!new_arr.includes(array[i])) {
+        count++
+        new_arr.push(array[i])
+    }
+}
+
+console.log(count)
