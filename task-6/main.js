06d6afb382c10124d0c4c9d8d00eea1c7e4f946d
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-01 19:56:42 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -13,3 +13,14 @@ let matrix_2 = [
 ]; 
 
 // Write code here. 
+let sum= 0
+
+for (let i = 0; i < matrix_1.length; i++){
+    for (let j = 0; j < matrix_2.length; j++){
+        if (matrix_2[i][j] == 1) {
+            sum += matrix_1[i][j]
+        }
+    }
+}
+
+console.log(sum)
