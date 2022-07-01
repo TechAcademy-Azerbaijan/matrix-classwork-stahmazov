7824e9529f40f7352603afb6283f14c0e7366120
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-01 20:09:56 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -3,3 +3,13 @@ let array_1 = [3, 1, 3, 4, 2, 4, 12]
 let array_2 = [4, 15, 43, 1, 15, 1]
 
 // Write code here. 
+
+let new_array = []
+
+for (let i = 0; i < array_1.length; i++){
+    if (!array_2.includes(array_1[i])) {
+        new_array.push(array_1[i])
+    }
+}
+
+console.log(...new_array)
